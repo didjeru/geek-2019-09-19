b96280929573802f49add527c100826c23fe2a0b
Di <55582048+didjeru@users.noreply.github.com>
2020-01-16 14:11:14 +0700

    Update 26.go

--- moretypes/26.go
+++ moretypes/26.go
@@ -5,12 +5,11 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
-	v1, v2 := 0, 1
-	return func() int {
-		z := v1
-		v1, v2 = v2, v2+z
-		return z
-	}
+    x, y := 1, 0
+    return func() int {
+        x, y = y, x+y
+        return x
+    }
 }
 
 func main() {
